2178c6bcce7d7150f3c552521391c77e8a81a320
akirago <akirasyoku1234@gmail.com>
2024-08-26 14:36:21 +0900

    edit genre

--- app/src/main/java/homelab/onlytake/RegisterGenreActivity.kt
+++ app/src/main/java/homelab/onlytake/RegisterGenreActivity.kt
@@ -1,13 +1,19 @@
 package homelab.onlytake
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import homelab.onlytake.database.AppDatabase
 import homelab.onlytake.database.Genre
 import homelab.onlytake.databinding.ActivityRegisterGenreBinding
+import homelab.onlytake.databinding.ViewholderRegisterResultBinding
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class RegisterGenreActivity : AppCompatActivity() {
@@ -20,12 +26,28 @@ class RegisterGenreActivity : AppCompatActivity() {
         binding = ActivityRegisterGenreBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         binding.btnRegisterGenre.setOnClickListener {
             val genreName = binding.etGenreName.text.toString()
             if (genreName.isNotEmpty()) {
                 saveGenreToDatabase(genreName)
             } else {
                 Toast.makeText(this, "Please enter a genre name", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        lifecycleScope.launch {
+            lifecycleScope.launch(Dispatchers.IO) {
+                val db = Room.databaseBuilder(
+                    applicationContext,
+                    AppDatabase::class.java, "app_database"
+                ).build()
+                val genres = db.genreDao().getAllGenres()
+                runOnUiThread {
+                    binding.genreList.adapter = GenreAdapter(genres)
+                    binding.genreList.layoutManager =
+                        LinearLayoutManager(this@RegisterGenreActivity)
+                }
             }
         }
     }
@@ -48,5 +70,27 @@ class RegisterGenreActivity : AppCompatActivity() {
                 binding.etGenreName.text.clear()
             }
         }
+    }
+}
+
+class GenreAdapter(private val itemList: List<Genre>) :
+    RecyclerView.Adapter<GenreAdapter.GenreViewHolder>() {
+
+
+    class GenreViewHolder(val binding: ViewholderRegisterResultBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenreViewHolder {
+        val binding = ViewholderRegisterResultBinding.inflate(LayoutInflater.from(parent.context))
+        return GenreViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: GenreViewHolder, position: Int) {
+        val item = itemList[position]
+        holder.binding.genreName.text = item.name
     }
 }
